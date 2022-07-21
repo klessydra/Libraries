f5953e8b4dc36651ebffe399cf0dd6bc12bbf142
Klessydra <angioli.marco@gmail.com>
2022-07-21 11:17:57 +0200

    values updates

--- cb_libs/inc/params_hybrid.hpp
+++ cb_libs/inc/params_hybrid.hpp
@@ -1,8 +1,8 @@
 // Header file with Disjoint params definition
 #define D 4
-#define AF 3
+#define AF 8
 #define K AF*D
 #define KA 12
 #define SINGLE_THREAD 0
-#define PERFORMANCE_COUNTER 1
+#define PERFORMANCE_COUNTER 0
 
