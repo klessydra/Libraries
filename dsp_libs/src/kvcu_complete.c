b584b1fc9e4e65adc576e7e2eb72623c58f15727
Abdallah Cheikh <abdallah.cheikh@uniroma1.it>
2024-08-18 15:40:41 +0200

    merged all VCU complete functions into kvcu_complete file

--- dsp_libs/src/kvcu_complete.c
+++ dsp_libs/src/kvcu_complete.c
@@ -318,7 +318,8 @@ void kdotp_complete(void *result, void* src1, void* src2, int size)
 	);
 }
 
-void kdotpps_complete(void *result, void* src1, void* src2, int size)
+
+void* kdotpps_complete(void *result, void* src1, void* src2, void* p_scal, int size)
 {
 	int SPMADDRA = spmaddrA;
 	int SPMADDRB = spmaddrB;
@@ -328,17 +329,44 @@ void kdotpps_complete(void *result, void* src1, void* src2, int size)
 	asm volatile(
 		"	kmemld %[SPMADDRA], %[srcA], %[sz];"
 		"	kmemld %[SPMADDRB], %[srcB], %[sz];"
-		"	csrw  0xBF0, %[sz]; "
-		"   csrwi 0xBE0, 2;"
+		"	csrw 0xBF0, %[sz]; "
+		"	csrw 0xBE0, %[p_scal]; "
 		"	kdotpps %[SPMADDRC], %[SPMADDRA], %[SPMADDRB];"
-		"	kmemstr %[result], %[SPMADDRC], %[sc_sz];" // if the parameter size was zero, then kmemstr will store nonsense value in th the memory
+		"	kmemstr %[result], %[SPMADDRC], %[sc_sz];"
 		:
-		:[SPMADDRA] "r" (SPMADDRA), [srcA] "r" (src1),
-		 [sz] "r" (size), [sc_sz] "r" (scalar_size),
+		:
+		 [SPMADDRA] "r" (SPMADDRA), [srcA] "r" (src1),
+		 [sz] "r" (size), [p_scal] "r" (p_scal), [sc_sz] "r" (scalar_size),
 		 [SPMADDRB] "r" (SPMADDRB), [srcB] "r" (src2),
          [SPMADDRC] "r" (SPMADDRC), [result] "r" (result)
 	);
+	return result;
 }
+
+void* kdotpps_complete_emul(void *result, void* src1, void* src2, void* p_scal, int size)
+{
+	int SPMADDRA = spmaddrA;
+	int SPMADDRB = spmaddrB;
+	int SPMADDRC = spmaddrC;
+	int SPMADDRD = spmaddrD;
+	char scalar_size = 4;
+	asm volatile(
+		"	kmemld %[SPMADDRA], %[srcA], %[sz];"
+		"	kmemld %[SPMADDRB], %[srcB], %[sz];"
+		"	csrw 0xBF0, %[sz]; "
+		"	kvmul %[SPMADDRC], %[SPMADDRA], %[SPMADDRB];"
+		"	ksrav %[SPMADDRC], %[SPMADDRC], %[p_scal];"
+		"	kvred %[SPMADDRC], %[SPMADDRC];"
+		"	kmemstr %[result], %[SPMADDRC], %[sc_sz];"
+		:
+		:[SPMADDRA] "r" (SPMADDRA), [srcA] "r" (src1),
+		 [sz] "r" (size), [p_scal] "r" (p_scal), [sc_sz] "r" (scalar_size),
+		 [SPMADDRB] "r" (SPMADDRB), [srcB] "r" (src2),
+         [SPMADDRC] "r" (SPMADDRC), [result] "r" (result)
+	);
+	return result;
+}
+
 
 void kbcast_complete(void *result, void* src1, void* src2, int size)
 {
@@ -357,6 +385,26 @@ void kbcast_complete(void *result, void* src1, void* src2, int size)
 		 [sz]       "r" (size),   [result] "r" (result)
 	);
 }
+
+void* kvcp_complete(void *result, void* src, int size)
+{
+	int SPMADDRA = spmaddrA;
+	int SPMADDRB = spmaddrB;
+	int SPMADDRC = spmaddrC;
+	int SPMADDRD = spmaddrD;
+	asm volatile(
+		"	kmemld %[SPMADDRA], %[src], %[sz];"
+		"	csrw 0xBF0, %[sz];"
+		"	kvcp %[SPMADDRB], %[SPMADDRA];"
+		"	kmemstr %[result], %[SPMADDRB], %[sz];"
+		:
+		:[SPMADDRA] "r" (SPMADDRA), [src] "r" (src), [sz] "r" (size),
+         [SPMADDRB] "r" (SPMADDRB), [result] "r" (result)
+		:
+	);
+	return result;
+}
+
 
 void kaddv_sw_loop_complete(void *result, void* src1, void* src2, int size, int SIMD_BYTES)
 {
